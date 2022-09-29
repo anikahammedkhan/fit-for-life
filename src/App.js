17f243af8fccd32f652df477d6cc258ca363d411
anikahammedkhan <anikkhan1105@gmail.com>
2022-09-29 22:03:58 +0600

    question added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <h1 className='text-4xl text-center my-5 text-blue-600 font-bold'>Fit For Life</h1>
       <p className='text-lg pl-6 font-semibold pb-4'>Select Today's Exercise :</p>
       <Exercises></Exercises>
+      <h1 className='text-4xl text-center my-5 text-cyan-500 font-bold'>Frequently Asked Question</h1>
       <Question></Question>
       <Footer></Footer>
     </div>
--- src/components/Exercises/Question/Question.js
+++ src/components/Exercises/Question/Question.js
@@ -1,13 +1,34 @@
 import React from 'react';
-
+import './Question.css';
 const Question = () => {
     return (
-        <div tabIndex={0} className="collapse collapse-open border border-base-300 bg-base-100 rounded-box">
-            <div className="collapse-title text-xl font-medium">
-                I have collapse-open className
+        <div className='question-container'>
+            <div className="collapse">
+                <input type="checkbox" className="peer" />
+                <div className="collapse-title bg-white text-black peer-checked:bg-info peer-checked:text-white">
+                    How Does React Work?
+                </div>
+                <div className="collapse-content  bg-white text-black peer-checked:bg-info peer-checked:text-white">
+                    <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code</p>
+                </div>
             </div>
-            <div className="collapse-content">
-                <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+            <div className="collapse mt-3">
+                <input type="checkbox" className="peer" />
+                <div className="collapse-title bg-white text-black peer-checked:bg-info peer-checked:text-white">
+                    How Does React Work?
+                </div>
+                <div className="collapse-content  bg-white text-black peer-checked:bg-info peer-checked:text-white">
+                    <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code</p>
+                </div>
+            </div>
+            <div className="collapse mt-3">
+                <input type="checkbox" className="peer" />
+                <div className="collapse-title bg-white text-black peer-checked:bg-info peer-checked:text-white">
+                    How Does React Work?
+                </div>
+                <div className="collapse-content  bg-white text-black peer-checked:bg-info peer-checked:text-white">
+                    <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code</p>
+                </div>
             </div>
         </div>
     );
